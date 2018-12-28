c8e78feb1eedf010fdef9c3a75c2fc8863d24ae5
lutoneta <anetadynek@hotmail.co.uk>
2018-12-28 21:02:57 +0000

    * .gitignore: Initial import
    * Playground.sln:
    * Program.cs:
    * Playground.csproj:
    * AssemblyInfo.cs:

---
+++ Playground/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Playground
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Zaczynamy");
+            //Console.ReadLine();
+            Rower dlybekObiekt = new Dlybek();
+            Rower kolarkaObiekt = new Kolarka();
+            Console.WriteLine("czy dlybek jest szybki? - " + dlybekObiekt.jestSzybki);
+            Console.WriteLine("czy kolarka ma przerzutki? - " + kolarkaObiekt.maPrzerzutki);
+            Console.WriteLine("Ile to jest 5 + 10: " + suma(5, 10));
+            Console.ReadLine();
+        }
+
+        //funkcja zwrotna
+        static int suma(int a, int b)
+        {
+            //int sum = x + y;
+            //string sentence = "The sum of " + x + " and " + y + " is " + sum + ".";
+            return a + b;
+        }
+    }
+
+    //Tworzymy nowa klase
+
+    public class Rower
+    {
+        // zmienne klasowe - nie okreslone
+        public Boolean maPrzerzutki;
+        public Boolean jestSzybki;
+    }
+
+    public class Dlybek : Rower
+    {
+        // konstruktor
+        public Dlybek()
+        {
+            // okreslone zmienne klasowe
+            maPrzerzutki = false;
+            jestSzybki = false;
+        }
+    }
+
+    public class Kolarka : Rower
+    {
+        public Kolarka()
+        {
+            maPrzerzutki = true;
+            jestSzybki = true;
+        }
+    }
+}
