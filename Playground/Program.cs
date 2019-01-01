78c6cd43b390c829721240ae83f1858f1f642697
lutoneta <anetadynek@hotmail.co.uk>
2019-01-01 16:49:20 +0000

    Dodany przyklad array

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -15,7 +15,14 @@ namespace Playground
             Console.WriteLine("czy kolarka ma przerzutki? - " + kolarkaObiekt.maPrzerzutki);
             Console.WriteLine("jakiego koloru jest Dlybek - " + Dlybek.kolor());
             Console.WriteLine("Ile to jest 5 + 10: " + suma(5, 10));
-            Console.ReadLine();
+            Console.WriteLine("jakiego koloru sa kola? - " + Rower.kolorKola());
+            przykladArray();
+
+            Console.WriteLine("\nCo?");
+            string reply = Console.ReadLine();
+            Console.WriteLine("AAAAA -> " + reply);
+
+
         }
 
         //funkcja zwrotna
@@ -24,6 +31,32 @@ namespace Playground
             //int sum = x + y;
             //string sentence = "The sum of " + x + " and " + y + " is " + sum + ".";
             return a + b;
+        }
+
+        static void przykladArray()
+        {
+            string[] array = new string[] { "uno", "duo", "tres", "quattro" };
+            Console.WriteLine("przyklad array - " + string.Join(", ", array));
+
+            /* initialize elements of array n */
+            for (int i = 0; i <array.Length; i++)
+            {
+                Console.WriteLine("I element: " + array[i] + " bingo!");
+            }
+
+            /* output each array element's value */
+            foreach (string j in array)
+            {
+                string newValue = j + " ha !";
+                Console.WriteLine("J element: " + newValue);
+            }
+
+            // sort
+
+            Array.Sort(array);
+            // write array
+            Console.WriteLine("Posortowane alfabetycznie");
+            foreach (string str in array) Console.Write(str + " ");
         }
     }
 
@@ -35,6 +68,11 @@ namespace Playground
         public Boolean maPrzerzutki;
         public Boolean jestSzybki;
         public int iloscKolek;
+
+        public static string kolorKola()
+        {
+            return "srebrny";
+        }
     }
 
     public class Dlybek : Rower
@@ -50,11 +88,12 @@ namespace Playground
             Console.WriteLine ("ile dlybek ma kol?- " + iloscKolek);
         }
 
-        public static string kolor()
+        public static string kolor()    
         {
             return "zieluny";
         }
     }
+
 
     public class Kolarka : Rower
     {
